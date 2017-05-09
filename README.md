a9917c0deb52c0a0d44e21858baf1dd1adc285cd
Graham <gmcallister@gatech.edu>
2017-05-09 00:18:31 -0400

    updated readme

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # kanjiHighlighter
 Chrome extension for visual aid while learning kanji from either WaniKani or Genki textbook series
+
+<p>TODO:
+<li>Redo highlighter unit tests
+<li>Add thumbnail & package to chrome store
+
+<p>Bugs:
+<li>Weird behavior for nested spans (check erin.ne.jp)
+
+<p>Limitation Notes:
+<li>Doesn't work on google maps
--- index.js
+++ index.js
@@ -4,16 +4,3 @@ angular.module('KanjiApp', ['ngMaterial', 'ngMessages'])
             .primaryPalette('red')
             .accentPalette('red');
     })
-
-
-//Limitation notes: doesn't work on google maps
-
-//Bugs: weird nesting html spans (check erin.ne.jp)
-
-
-//TODO:
-//      Redo unit tests to get all passing
-//      Add a thumbnail
-//      Upload to github
-//      Package in chrome store
-//      Add readme
